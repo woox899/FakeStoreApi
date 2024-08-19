17cf28a751c7831f6a8d9db8dc69161465429da5
woox899 <woox899@gmail.com>
2024-08-19 15:26:58 +0400

    Initial Commit

---
+++ FakeStoreApi/FakeStoreApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApiApp.swift
+//  FakeStoreApi
+//
+//  Created by Андрей Бабкин on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
